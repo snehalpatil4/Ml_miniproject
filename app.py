5b789e8a1aaee033688cefdd09689431d3a32d7e
Snehal Patil <51523547+snehalpatil4@users.noreply.github.com>
2021-05-24 15:52:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,request
+import numpy as np
+from sklearn.decomposition import PCA
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('linear_regression.pkl', 'rb'))
+scaler=pickle.load(open('scaler.pkl','rb'))
+pca_x=pickle.load(open('pca_model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('Home.HTML')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    print(final_features)
+    scaled_data=scaler.transform(final_features)
+    print(scaled_data)
+    x_pca = pca_x.transform(scaled_data)
+    print(x_pca)
+    prediction = model.predict(x_pca)
+    print(prediction)
+    output = round(prediction[0], 5)
+    print(output)
+
+    return render_template('home.html', prediction_text='Share will close on  INR {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
